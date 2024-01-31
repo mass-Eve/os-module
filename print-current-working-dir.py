9848abf7c3614a21ea2264606ee0002353198c75
mass-eve <detoxedankur@gmail.com>
2024-01-31 21:02:41 +0530

    print current working directory

--- print-current-working-dir.py
+++ print-current-working-dir.py
@@ -1,13 +1,3 @@
-# os Module Python
-
-First, import os Module using the `import` keyword
-```python
-import os
-```
-
-## 1 - Print The Current Working Directory ~
-
-```python
 import os
 
 print('Want to know your current working directory ?')
@@ -16,4 +6,3 @@ if (ch == 'Y' or 'y'):
     print(f'This is your current working directory - {os.getcwd()}')
 else:
     pass
-```
